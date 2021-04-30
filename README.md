89b5526957b7711356ae7b8aae8d1f9141dc9e04
wvwall <waltervelardo@gmail.com>
2021-04-30 14:49:48 +0200

    Completed Header

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 php-google-faq
+Descrizione:
+Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" /> 
+    <title>Domande frequenti – Privacy e termini – Google</title>
+    <link rel="shortcut icon" href="./dist/img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./dist/css/master.css">
+    
+    
+</head>
+<body>
+    <header>
+        <section>
+            <div class="navbar-top">
+                <div class="left-top">
+                    <img src="./dist/img/logo.png" alt="">
+                    <h1>Privacy e termini</h1>
+                </div>
+                <div class="right-top">
+                    <svg class="gb_We" focusable="false" viewBox="0 0 24 24">
+                    <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path></svg>
+                    <img src="./dist/img/user.jpg" alt="">
+                </div>
+            </div> 
+            <div class="navbar-bottom">
+                <ul class="list">
+                    <li><a href="">Introduzione</a></li>
+                    <li><a href="">Norme sulla privacy</a></li>
+                    <li><a href="">Termini di servizio</a></li>
+                    <li><a href="">Tecnologie</a></li>
+                    <li><a id="active" href="">Domande frequenti</a></li>
+                </ul>
+            </div>
+        </section>
+    </header>
+     
+    <main>
+        <section>
+            <div class="container-faq"></div>
+        </section>
+    </main>
+
+    <footer>
+       
+    </footer>
+
+
+   
+</body>
+</html>
